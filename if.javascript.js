1d1a91f5f9a45eb5b7da67727867661dfe472d8d
amelegech <acheklie@mum.edu>
2019-09-02 14:02:10 -0500

    Create if.javascript.js

---
+++ if.javascript.js
@@ -0,0 +1,22 @@
+/*var dayOf =7;
+var weather = "sunny";
+if(dayOf>5){
+console.log("have a fan!");
+}
+if(dayOf===7 && weather==="sunny"){
+console.log("lets watch movie");
+}*/
+
+var today = 1;
+var weather = "rainy";
+if(today===1 && weather==="rainy"){
+console.log("get an umbrela!")
+
+var game = 6;
+var gameOver = 7;
+if(game<=6){
+console.log("Bingo");
+}
+if(gameOver===7){
+console.log("try again!");
+}
